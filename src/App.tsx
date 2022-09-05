27def99fe7ee83e3fb03177d51c769b2bc7f9676
navanjani <navanjani@outlook.com>
2022-09-05 13:48:58 +0200

    Adding sign up page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { COMPANY_JOBS_ROUTE, HOME_PAGE_ROUTE } from './config/routes';
+import {
+  COMPANY_JOBS_ROUTE,
+  HOME_PAGE_ROUTE,
+  SIGNUP_PAGE_ROUTE,
+} from './config/routes';
 import HomePage from './pages/HomePage';
 import './App.scss';
 import ClientNavbar from './components/ClientNavbar';
 import CompanyJobsPage from './pages/CompanyJobsPage';
+import SignUpPage from './pages/SignUpPage';
 
 function App() {
   return (
@@ -13,6 +18,7 @@ function App() {
       <Routes>
         <Route path={HOME_PAGE_ROUTE} element={<HomePage />} />
         <Route path={COMPANY_JOBS_ROUTE} element={<CompanyJobsPage />} />
+        <Route path={SIGNUP_PAGE_ROUTE} element={<SignUpPage />} />
       </Routes>
     </div>
   );
