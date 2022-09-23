4009ab21f9fddb61f55554160819dc1a8f3d7663
navanjani <navanjani@outlook.com>
2022-09-23 08:08:13 +0200

    Toasts off by default

--- src/helpers/apiError.ts
+++ src/helpers/apiError.ts
@@ -1,33 +1,28 @@
 import { toast } from 'react-toastify';
-import { setMessage } from '../store/appState/slice';
 import { AppDispatch } from '../store';
+import { setMessage } from '../store/appState/slice';
 
-export const apiError = (dispatch: AppDispatch, error: any) => {
+export const apiError = (dispatch: AppDispatch, error: any, showToast: boolean = false) => {
+  let errorMessage = 'Something went wrong!';
   if (error.response) {
     /* eslint no-console: 0 */
     console.log(error.response);
     if (error.response.data) {
-      toast.error(error.response.data.message);
-      dispatch(
-        setMessage({
-          variant: 'danger',
-          dismissable: true,
-          text: error.response.data.message,
-        }),
-      );
-    } else {
-      toast.error('Something went wrong!');
+      errorMessage = error.response.data.message;
     }
   } else {
     /* eslint no-console: 0 */
     console.log(error.message);
-    toast.error(error.message);
-    dispatch(
-      setMessage({
-        variant: 'danger',
-        dismissable: true,
-        text: error.message,
-      }),
-    );
+    errorMessage = error.message;
   }
+  if (showToast) {
+    toast.error(errorMessage);
+  }
+  dispatch(
+    setMessage({
+      variant: 'danger',
+      dismissable: true,
+      text: errorMessage,
+    }),
+  );
 };
